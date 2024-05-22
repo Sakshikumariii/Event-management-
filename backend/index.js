108acbce589b18eb5213481c9c8a810803e36495
Sakshi Kumari <sakshismp2003@gmail.com>
2024-05-22 16:02:19 +0530

    second

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 require('dotenv').config();
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://event-management-rrw4.vercel.app/']
+    origin: ['http://localhost:3000', 'https://event-management-t2gv-sakshis-projects-8cf90f76.vercel.app/']
   }));
 
 const mongoose = require('mongoose');
